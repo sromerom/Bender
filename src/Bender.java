91d2d5a57f8b897950411f772ebd99ea949de067
Samuel Romero Marin <sromerom@esliceu.net>
2020-02-19 16:41:15 +0100

    Fitxers inicials

---
+++ src/Bender.java
@@ -0,0 +1,18 @@
+class Bender {
+    // Constructor: ens passen el mapa en forma d'String
+    public Bender(String mapa) {
+    }
+
+    // Navegar fins a l'objectiu («$»).
+    // El valor retornat pel mètode consisteix en una cadena de
+    // caràcters on cada lletra pot tenir
+    // els valors «S», «N», «W» o «E»,
+    // segons la posició del robot a cada moment.
+    public String run() {
+        return "";
+    }
+
+    public String bestRun() {
+        return "";
+    }
+}
